96904c517e3e2bd0c709f8a6e905ac9956f6aeb2
skay <skai-zhang@hotmail.com>
2023-08-21 12:09:47 +0800

    optimized code

--- frontend/src/components/hostTerminal.vue
+++ frontend/src/components/hostTerminal.vue
@@ -7,6 +7,8 @@ import "xterm/css/xterm.css";
 import { Terminal } from "xterm";
 import { FitAddon } from 'xterm-addon-fit';
 import { AttachAddon } from 'xterm-addon-attach';
+import { CanvasAddon } from 'xterm-addon-canvas';
+import { WebglAddon } from 'xterm-addon-webgl';
 
 export default {
     name: "HostTerminal",
@@ -17,12 +19,15 @@ export default {
     }),
     methods: {
         init() {
-            // 1.xterm终端初始化
             this.term = new Terminal()
             // 创建WebSocket连接
             this.socket = new WebSocket(this.socketURI)
             // 加载WebSocket插件
             this.term.loadAddon(new AttachAddon(this.socket))
+            // 加载Canvas渲染
+            this.term.loadAddon(new CanvasAddon())
+            // 加载WebGL渲染
+            this.term.loadAddon(new WebglAddon())
             // 加载窗口自适应插件
             const fitAddon = new FitAddon()
             this.term.loadAddon(fitAddon)
@@ -32,140 +37,12 @@ export default {
             fitAddon.fit()
             // 输入聚焦
             this.term.focus()
-            // 监听输入事件
-            this.inputEvent()
         },
         close() {
+            // 关闭连接
             if (this.socket) this.socket.close()
+            // 销毁终端
             if (this.term) this.term.dispose()
-        },
-        inputEvent() {
-            const TERMINAL_INPUT_KEY = {
-                BACK: 8, // 删除键
-                TAB: 9, // TAB键
-                ENTER: 13, // 回车键
-                UP: 38, // 方向上键
-                DOWN: 40, // 方向下键
-                LEFT: 37, // 方向左键
-                RIGHT: 39, // 方向右键
-            };
-            let prefix = 0;
-            let inputText = "";
-            let currentIndex = 0;
-            let inputTextList = [];
-            this.term.onKey((e) => {
-                const { key, domEvent } = e;
-                const { keyCode, altKey, altGraphKey, ctrlKey, metaKey } = domEvent;
-                // 计算前缀长度
-                if (inputText == "" && prefix == 0) prefix = this.term._core.buffer.x;
-
-                const printAble = !(altKey || altGraphKey || ctrlKey || metaKey); // 禁止相关按键
-                const totalOffsetLength = inputText.length + prefix; // 总偏移量
-                const currentOffsetLength = this.term._core.buffer.x; // 当前x偏移量
-
-                switch (keyCode) {
-                    // 删除键
-                    case TERMINAL_INPUT_KEY.BACK: {
-                        if (currentOffsetLength > prefix) {
-                            const cursorOffSetLength = this.getCursorOffsetLength(totalOffsetLength - currentOffsetLength, "\x1b[D"); // 保留原来光标位置
-
-                            this.term._core.buffer.x = currentOffsetLength - 1;
-                            this.term.write("\x1b[?K" + inputText.slice(currentOffsetLength - prefix));
-                            this.term.write(cursorOffSetLength);
-                            inputText = `${inputText.slice(0, currentOffsetLength - prefix - 1)}${inputText.slice(
-                                currentOffsetLength - prefix
-                            )}`;
-                        }
-                        break;
-                    }
-                    // TAB键
-                    case TERMINAL_INPUT_KEY.TAB:{
-                        // TODO
-                    }
-                    // 回车键
-                    case TERMINAL_INPUT_KEY.ENTER: {
-                        this.term.write("\r\n");
-                        console.log("inputText", inputText);
-
-                        if (!inputText.trim()) {
-                            // this.term.prompt();
-                            return;
-                        }
-
-                        if (inputTextList.indexOf(inputText) === -1) {
-                            inputTextList.push(inputText);
-                            currentIndex = inputTextList.length;
-                        }
-
-                        // this.term.prompt();
-                        inputText = "";
-                        break;
-                    }
-                    // 方向上键
-                    case TERMINAL_INPUT_KEY.UP: {
-                        if (!inputTextList[currentIndex - 1]) break;
-
-                        const offsetLength = this.getCursorOffsetLength(inputText.length, "\x1b[D");
-
-                        inputText = inputTextList[currentIndex - 1];
-                        this.term.write(offsetLength + "\x1b[?K");
-                        this.term.write(inputTextList[currentIndex - 1]);
-                        this.term._core.buffer.x = totalOffsetLength;
-                        currentIndex--;
-
-                        break;
-                    }
-                    // 方向下键
-                    case TERMINAL_INPUT_KEY.DOWN: {
-                        if (!inputTextList[currentIndex + 1]) break;
-
-                        const offsetLength = this.getCursorOffsetLength(inputText.length, "\x1b[D");
-
-                        inputText = inputTextList[currentIndex + 1];
-                        this.term.write(offsetLength + "\x1b[?K");
-                        this.term.write(inputTextList[currentIndex + 1]);
-                        this.term._core.buffer.x = totalOffsetLength;
-                        currentIndex++;
-
-                        break;
-                    }
-                    // 方向左键
-                    case TERMINAL_INPUT_KEY.LEFT: {
-                        if (currentOffsetLength > prefix) {
-                            this.term.write(key); // '\x1b[D'
-                        }
-                        break;
-                    }
-                    // 方向右键
-                    case TERMINAL_INPUT_KEY.RIGHT:{
-                        if (currentOffsetLength < totalOffsetLength) {
-                            this.term.write(key); // '\x1b[C'
-                        }
-                        break;
-                    }
-                    default: {
-                        if (!printAble) break;
-                        if (totalOffsetLength >= this.term.cols) break;
-                        if (currentOffsetLength >= totalOffsetLength) {
-                            this.term.write(key);
-                            inputText += key;
-                            break;
-                        }
-                        let cursorOffSetLength = this.getCursorOffsetLength(totalOffsetLength - currentOffsetLength, "\x1b[D");
-                        this.term.write("\x1b[?K" + `${key}${inputText.slice(currentOffsetLength - prefix)}`);
-                        this.term.write(cursorOffSetLength);
-                        inputText = inputText.slice(0, currentOffsetLength) + key + inputText.slice(totalOffsetLength - currentOffsetLength);
-                        break;
-                    }
-                }
-            });
-        },
-        getCursorOffsetLength(offsetLength, subString) {
-            let cursorOffsetLength = "";
-            for (let offset = 0; offset < offsetLength; offset++) {
-                cursorOffsetLength += subString;
-            }
-            return cursorOffsetLength;
         },
     }
 };
@@ -176,6 +53,7 @@ export default {
 }
 
 .xterm:deep(.terminal) {
-    height: calc(100vh - 50px);
+    height: calc(100vh - 60px);
+    padding: 5px;
 }
 </style>
